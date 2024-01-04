00debed2a26b7d820989172e25686501e1ada83d
aslmhamed <aslamhamed999@gmail.com>
2024-01-04 08:19:35 -0800

    all tasks

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    import sys
+
+    argument = len(sys.argv) -1 # -1 for the file name
+    if argument == 0:
+        print("0 arguments.")
+    elif argument == 1:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(argument))
+    for i in range(argument):
+        print("{}: {}".format(i + 1, sys.argv[i + 1]))
+
---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    import sys
+    argument = len(sys.argv) -1
+
+    sum = 0
+    for i in range(argument):
+        sum += int(sys.argv[i + 1])
+    print(sum)
+
+
