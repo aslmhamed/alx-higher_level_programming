671951bf479bfdd84d9ca87140f87b74f98aee84
aslmhamed <aslamhamed999@gmail.com>
2024-01-07 14:49:25 -0800

    from task 6 to task 9

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+def multiple_returns(sentence):
+    len_sentence = len(sentence)
+    if len_sentence == 0:
+        return (0, None)
+    else:
+        return (len_sentence, sentence[0])
