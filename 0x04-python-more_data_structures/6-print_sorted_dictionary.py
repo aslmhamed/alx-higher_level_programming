16c248c2a20a024618a7208215982870648642a7
aslmhamed <aslamhamed999@gmail.com>
2024-01-10 14:06:00 -0800

    tasks 7 & 8 & 9

---
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+
+def print_sorted_dictionary(a_dictionary):
+    sorted_dict = sorted(a_dictionary.keys())
+    for keys in sorted_dict:
+        print("{:s} : {}".format(keys, a_dictionary[key]))
