89324419b644066e5a396fa05f20eb7143741baf
aslmhamed <aslamhamed999@gmail.com>
2024-01-22 11:20:54 -0800

    fixed pycode error

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -2,9 +2,9 @@
 def safe_print_list_integers(my_list=[], x=0):
     try:
         count = 0
-        for i in range (x):
+        for i in range(x):
             if isinstance(my_list[i], int):
-                count +=1
+                count += 1
                 print("{:d}".format(my_list[i]), end="")
     except TypeError:
         print("Type must be integer")
