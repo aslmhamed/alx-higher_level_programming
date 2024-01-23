22625efe91076d912640d55e572471308da626f8
aslmhamed <aslamhamed999@gmail.com>
2024-01-22 17:03:04 -0800

    forgot to add the sys lib :( not fifth today

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
+import sys
+
 
 def safe_function(fct, *args):
     try:
         safe_func = fct(*args)
-    except (TypeError, NameError, ZeroDivisonError, IndexError) as e:
+    except (TypeError, NameError, ZeroDivisionError, IndexError) as e:
         print("Exception: {}".format(e), file=sys.stderr)
         return None
     else:
