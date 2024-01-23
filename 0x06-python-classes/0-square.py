89bd2b6417829de766d7a1ff7fbcf8ff55239760
aslmhamed <aslamhamed999@gmail.com>
2024-01-23 12:58:38 -0800

    Empty Square Class

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,3 +1,10 @@
 class Square:
-    """An empty class square that defines a square """
+    """An empty class square that defines a square.
+
+    Thise class server as placeholder or templete for future use.
+
+    Note:
+        This class is intentionally empty
+
+    """
     pass
