66f61c308536d2949920857dbc098a91c0ca5f98
aslmhamed <aslamhamed999@gmail.com>
2024-01-23 14:36:50 -0800

    hopefully no red checks :(

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+"""
+Create a class named Square that defines a private instance attribute of: size.
+"""
+
+
+class Square:
+    """defines class of square."""
+    def __init__(self, size):
+        self.__size = size
